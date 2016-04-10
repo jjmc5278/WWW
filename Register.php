fb0c2f8488aec76a6c65f5d063800e90f29fbef1
jjmc5278 <casey112@uwindsor.ca>
2016-04-10 05:58:03 -0400

    Create Register.php

---
+++ Register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE>
+<html>
+
+<head>
+    <title>BlogLife</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- This is how you comment?-->
+    <style>
+        .jumbotron {
+            height: 200px;
+        }
+    </style>
+  </head>
+<body>
+
+<nav class="navbar navbar-dark bg-inverse">
+    <ul class="nav navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="index.php">
+                Home
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="login.php">
+                Log In
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contact.php">
+                Contact
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="Register.php">
+                Register
+            </a>
+        </li>
+    </ul>
+
+</nav>
+
+
+
+<div class="jumbotron" style="background-color: lightsteelblue">
+    <div class="container">
+        <h1>Windsor Bloglife</h1>
+        <p class="lead blog-description">
+            A blog about the life of a University of Windsor student!
+            How cool!
+        </p>
+    </div>
+</div>
+</body>
+</html>
