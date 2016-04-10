062cd5de155b7e6f39c038b633344221eb3ae54d
jjmc5278 <casey112@uwindsor.ca>
2016-04-10 00:12:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="logIn.php">
+            <a class="nav-link" href="login.php">
                 Log In
             </a>
         </li>
